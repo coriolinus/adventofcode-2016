dc6d5a0b1e53ad883c152c61ba0682936e8fad18
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-01-02 16:49:20 +0100

    Add cartesian math

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -19,13 +19,14 @@
 //! R5, L5, R5, R3 leaves you 12 blocks away.
 //! How many blocks away is Easter Bunny HQ?
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub enum Rotation {
     Left,
     Right,
 }
 
-pub type Directions = Vec<(Rotation, usize)>;
+pub type Direction = (Rotation, usize);
+pub type Directions = Vec<Direction>;
 
 pub fn parse(input: &str) -> Directions {
     let mut results = Vec::with_capacity(input.len() / 4);
@@ -33,13 +34,126 @@ pub fn parse(input: &str) -> Directions {
     for token in input.split(", ") {
         let (dir_char, dist) = token.split_at(1);
 
-        results.push((match dir_char {
+        let direction = match dir_char {
             "L" => Rotation::Left,
             "R" => Rotation::Right,
             _ => panic!("Invalid input; invalid rotation char"),
-        },
-                      usize::from_str_radix(dist, 10).expect("Invalid input; unparseable distance")));
+        };
+
+        results.push((direction,
+                      usize::from_str_radix(dist, 10)
+                        .expect("Invalid input; unparseable distance")));
     }
 
     results
+}
+
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum Facing {
+    North,
+    South,
+    East,
+    West,
+}
+
+impl Facing {
+    pub fn turn(&self, r: Rotation) -> Facing {
+        use Facing::*;
+        match r {
+            Rotation::Left => {
+                match *self {
+                    North => West,
+                    West => South,
+                    South => East,
+                    East => North,
+                }
+            }
+            Rotation::Right => {
+                match *self {
+                    North => East,
+                    East => South,
+                    South => West,
+                    West => North,
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Coordinates {
+    facing: Facing,
+    x: isize,
+    y: isize,
+}
+
+impl Default for Coordinates {
+    fn default() -> Coordinates {
+        Coordinates {
+            facing: Facing::North,
+            x: 0,
+            y: 0,
+        }
+    }
+}
+
+impl Coordinates {
+    pub fn new(facing: Facing, x: isize, y: isize) -> Coordinates {
+        Coordinates {
+            facing: facing,
+            x: x,
+            y: y,
+        }
+    }
+
+    pub fn add(&self, direction: Direction) -> Coordinates {
+        let (rotation, distance) = direction;
+        let distance = distance as isize;
+
+        let facing = self.facing.turn(rotation);
+
+        let (x, y) = {
+            use Facing::*;
+            match facing {
+                North => (self.x, self.y + distance),
+                East => (self.x + distance, self.y),
+                South => (self.x, self.y - distance),
+                West => (self.x - distance, self.y),
+            }
+        };
+
+        Coordinates {
+            facing: facing,
+            x: x,
+            y: y,
+        }
+    }
+
+    pub fn follow(&self, directions: Directions) -> Coordinates {
+        unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_default() {
+        // test both rotation directions
+        assert!(Coordinates::default().add((Rotation::Right, 1)) ==
+                Coordinates::new(Facing::East, 1, 0));
+
+        assert!(Coordinates::default().add((Rotation::Left, 1)) ==
+                Coordinates::new(Facing::West, -1, 0));
+
+        // test a different distance
+        assert!(Coordinates::default().add((Rotation::Right, 5)) ==
+                Coordinates::new(Facing::East, 5, 0));
+
+        // test a different initial value
+        assert!(Coordinates::new(Facing::East, 0, -5).add((Rotation::Left, 5)) ==
+                Coordinates::default());
+    }
 }
