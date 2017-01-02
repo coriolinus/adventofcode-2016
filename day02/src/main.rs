bbf6946a8e483be0116bb864fb1f04bda94e26f7
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-01-02 23:42:53 +0100

    Call decode function from executable

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,5 +1,11 @@
 extern crate day02lib;
+use day02lib::decode;
+
+extern crate util;
+use util::get_lines;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Enter code lines here:");
+    let lines = get_lines();
+    println!("Decoded: {:?}", decode(&lines));
 }
--- util/src/lib.rs
+++ util/src/lib.rs
@@ -1,7 +1,14 @@
 use std::io;
+use std::io::prelude::*;
 
 pub fn get_line() -> String {
     let mut buffer = String::new();
     io::stdin().read_line(&mut buffer).expect("Could not read from Stdin");
-    return buffer;
+    buffer
+}
+
+pub fn get_lines() -> String {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).expect("Could not read from Stdin");
+    buffer
 }
