4d838599cfde5481239aef38207de31d902958e5
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-01-03 13:05:13 +0100

    Add first star functionality to main

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,5 +1,15 @@
 extern crate day03lib;
+use day03lib::{parse_lines_as_usize, count_valid};
+
+extern crate util;
+use util::get_lines;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Enter space-separated positive integer triangles, one per line:");
+    let lines = get_lines();
+    if let Some(triangles) = parse_lines_as_usize(&lines) {
+        println!("{} Valid triangles", count_valid(triangles));
+    } else {
+        println!("Parse error; no lines found")
+    }
 }
