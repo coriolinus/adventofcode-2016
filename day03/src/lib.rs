5526929877dbc701dd76b91fc96fbd2575339c02
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-01-03 13:00:26 +0100

    Add utility methods to ease main function

--- day03/src/lib.rs
+++ day03/src/lib.rs
@@ -27,6 +27,35 @@ pub fn is_possible<T>(t: &Triangle<T>) -> bool
     t[0] + t[1] > t[2]
 }
 
+pub fn parse_line<T, F>(input: &str, parser: &F) -> Option<Triangle<T>>
+    where F: Fn(&str) -> Option<T>,
+          T: Copy
+{
+    if let Some(items) = input.split_whitespace().map(parser).collect::<Option<Vec<T>>>() {
+        if items.len() == 3 {
+            return Some([items[0], items[1], items[2]]);
+        }
+    }
+    None
+}
+
+pub fn parse_lines<T, F>(input: &str, parser: &F) -> Option<Vec<Triangle<T>>>
+    where F: Fn(&str) -> Option<T>,
+          T: Copy
+{
+    input.lines().map(|line| parse_line(line, parser)).collect()
+}
+
+pub fn parse_lines_as_usize(input: &str) -> Option<Vec<Triangle<usize>>> {
+    parse_lines(input, &|s: &str| s.parse::<usize>().ok())
+}
+
+pub fn count_valid<T>(ts: Vec<Triangle<T>>) -> usize
+    where T: Copy + Ord + ::std::ops::Add<Output = T>
+{
+    ts.iter().filter(|t| is_possible(t)).count()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
