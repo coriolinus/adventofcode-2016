3bf04763ed1650ef54ab8386bc718880e43564b8
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-01-04 13:00:46 +0100

    Update executable to decrypt, locate target string.
    Searching for the text I expected didn't work at first, so the new
    behavior is just to output all entries. This is verbose, so recommended
    usage becomes:
    
    `cat .\input.txt | cargo run | grep -i north`

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,5 +1,5 @@
 extern crate day04lib;
-use day04lib::sum_valid_sectors;
+use day04lib::{sum_valid_sectors, find_np_lines, validate, decrypt};
 
 extern crate util;
 use util::get_lines;
@@ -8,4 +8,16 @@ fn main() {
     println!("Enter room lines here:");
     let lines = get_lines();
     println!("Sum of valid sectors: {:?}", sum_valid_sectors(&lines));
+    println!();
+    println!("Rooms mentioning the North Pole:");
+    for (name, sector) in find_np_lines(&lines) {
+        println!("  {}  {}", name, sector);
+    }
+    println!();
+    println!("All valid rooms:");
+    for line in lines.lines() {
+        if validate(line) {
+            println!("{} ({})", decrypt(line).unwrap(), line);
+        }
+    }
 }
