ab19376f9fb5aee44d6a1869dd39268e25f8d7d8
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-01-04 12:04:42 +0100

    Update main file to run program

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,5 +1,11 @@
 extern crate day04lib;
+use day04lib::sum_valid_sectors;
+
+extern crate util;
+use util::get_lines;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Enter room lines here:");
+    let lines = get_lines();
+    println!("Sum of valid sectors: {:?}", sum_valid_sectors(&lines));
 }
