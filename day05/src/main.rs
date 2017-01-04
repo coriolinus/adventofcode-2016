14c6353de0d0e6d97ea0bac79bb3b4e753df9f18
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-01-04 13:44:04 +0100

    Add main implementation

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,5 +1,14 @@
 extern crate day05lib;
+use day05lib::GetPassword;
+
+use std::io::{Write, stdout};
 
 fn main() {
-    println!("Hello, world!");
+    const INPUT: &'static str = "ugkcyxxp";
+    println!("Hashing...");
+    for pw_char in GetPassword::new(INPUT).take(8) {
+        print!("{}", pw_char);
+        stdout().flush().expect("failed to flush stdout");
+    }
+    println!();
 }
