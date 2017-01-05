d7b3a1f50f6950f1daae63f2c8788fa32c053617
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-01-05 13:29:29 +0100

    Add code for executable

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,5 +1,11 @@
 extern crate day06lib;
+use day06lib::count_most_frequent;
+
+extern crate util;
+use util::get_lines;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Enter problem lines:");
+    let lines = get_lines();
+    println!("{}", count_most_frequent(&lines));
 }
