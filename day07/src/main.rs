44755d6980bbba287749ecfd6d6b87c522f5720a
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-01-05 21:20:44 +0100

    Add SSL support to binary

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,5 +1,5 @@
 extern crate day07lib;
-use day07lib::supports_tls;
+use day07lib::{supports_tls, supports_ssl};
 
 extern crate util;
 use util::get_lines;
@@ -8,8 +8,13 @@ fn count_supports_tls(lines: &str) -> usize {
     lines.lines().filter(|line| supports_tls(line.trim())).count()
 }
 
+fn count_supports_ssl(lines: &str) -> usize {
+    lines.lines().filter(|line| supports_ssl(line.trim())).count()
+}
+
 fn main() {
     println!("Enter ipv7 addresses:");
     let lines = get_lines();
     println!("ABBA count: {}", count_supports_tls(&lines));
+    println!("SSL count:  {}", count_supports_ssl(&lines));
 }
