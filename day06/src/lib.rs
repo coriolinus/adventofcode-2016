29e4ca2ebe6501901465eb6dbf6d3b225a643ab7
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-01-05 13:17:28 +0100

    Add function to get output given lines input, and test

--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -64,6 +64,29 @@ pub fn transpose<T: Copy + Default>(input: &Vec<Vec<T>>) -> Vec<Vec<T>> {
     output
 }
 
+/// Compose a string in which each character is the most common from the input lines.
+///
+/// Every input line must have an equal number of characters for this to work right.
+pub fn count_most_frequent(lines: &str) -> String {
+    let input = lines.lines()
+        .map(|line| line.trim())
+        .filter(|line| !line.is_empty())
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let transposed = transpose(&input);
+
+    let mut output = String::with_capacity(transposed.len());
+
+    for chars in transposed {
+        output.push(
+            Counter::init(chars).most_common().next().expect("At least one row required!").0
+        );
+    }
+
+    output
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -89,5 +112,32 @@ mod tests {
     #[test]
     fn test_double_transpose_is_noop() {
         assert!(transpose(&transpose(&get_transpose_example())) == get_transpose_example());
+    }
+
+    fn get_lines_example() -> String {
+        "\
+eedadn\n\
+drvtee\n\
+eandsr\n\
+raavrd\n\
+atevrs\n\
+tsrnev\n\
+sdttsa\n\
+rasrtv\n\
+nssdts\n\
+ntnada\n\
+svetve\n\
+tesnvt\n\
+vntsnd\n\
+vrdear\n\
+dvrsen\n\
+enarar\n\
+        "
+            .to_string()
+    }
+
+    #[test]
+    fn test_given_example() {
+        assert!(&count_most_frequent(&get_lines_example()) == "easter");
     }
 }
