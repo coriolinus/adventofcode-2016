c077fa25dc150eb4173d212192681a2f2e965566
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-01-05 17:44:27 +0100

    Add function to split a string by bracketedness

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -22,3 +22,70 @@
 //!    within a larger string).
 //!
 //! How many IPs in your puzzle input support TLS?
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Day07Err {
+    UnmatchedBrackets,
+    NestedBrackets(usize, usize),
+    ReversedBrackets(usize, usize),
+}
+
+/// Split a string into a list of substrings, split by square brackets.
+///
+/// Return the section and whether or not it occurs within brackets.
+///
+/// Nested or unmatched brackets cause this to return None.
+pub fn split_brackets<'a>(input: &'a str) -> Result<Vec<(&'a str, bool)>, Day07Err> {
+    // ensure we have the same number of brackets
+    if input.chars().filter(|&c| c == '[').count() != input.chars().filter(|&c| c == ']').count() {
+        return Err(Day07Err::UnmatchedBrackets);
+    }
+
+    // otherwise, match them into sections, and check those
+    let open_brackets = input.match_indices('[').map(|t| t.0);
+    let close_brackets = input.match_indices(']').map(|t| t.0);
+    let bracket_sections = open_brackets.zip(close_brackets)
+        .collect::<Vec<_>>();
+
+    // validate that we have sane brackets
+    for &(start, end) in bracket_sections.iter() {
+        if start >= end {
+            return Err(Day07Err::ReversedBrackets(start, end));
+        }
+    }
+    for window in bracket_sections.windows(2) {
+        let (start1, end1) = window[0];
+        let (start2, _) = window[1];
+
+        if end1 >= start2 {
+            return Err(Day07Err::NestedBrackets(start1, start2));
+        }
+    }
+
+    let mut result = Vec::new();
+    let mut index = 0;
+
+    // for each bracketed section, we append two sections:
+    // those elements before the opening bracket,
+    // and those within
+    //
+    // then, we append a section containing everything after the final bracket
+    //
+    // Example: the string `abba[mnop]qrst`
+    // will map start, end once, at (4, 9)
+    // we create three substrings: [[0..4], [5..9], [10..14]]
+    for (start, end) in bracket_sections {
+        if start > index {
+            // true if the bracket wasn't the first character
+            result.push((&input[index..start], false));
+        }
+        if (end - start) > 1 {
+            // true if there are characters between the brackets
+            result.push((&input[(start + 1)..end], true))
+        }
+        index = end + 1;
+    }
+    result.push((&input[index..], false));
+
+    Ok(result)
+}
