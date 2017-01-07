88c5ee6cdc94f54fac6ea0d7d9cef6bbbace7052
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-01-07 23:31:28 +0100

    Update to handle second part of question

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -8,12 +8,19 @@ fn main() {
     println!("Enter instructions:");
     let lines = get_lines();
     if let Some(instructions) = parse_lines(&lines) {
-        if let Ok((bots, _)) = process(instructions) {
+        if let Ok((bots, outputs)) = process(instructions) {
             if let Some(bot) = find_bot_handling(&bots, 61, 17) {
                 println!("Bot {} handles 61, 17", bot);
             } else {
                 println!("Couldn't find bot handling 61, 17");
             }
+
+            let zero = outputs.get(&0).unwrap().iter().next().unwrap();
+            let one = outputs.get(&1).unwrap().iter().next().unwrap();
+            let two = outputs.get(&2).unwrap().iter().next().unwrap();
+
+            let product = zero * one * two;
+            println!("Product of bins [0]*[1]*[2]={}", product);
         } else {
             println!("Failed to process instructions");
         }
