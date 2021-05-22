56463866a397f6a277089438a39f4b2242630170
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-05-22 10:25:30 +0200

    solve day13
    I was a bit nervous about off-by-1 errors, particularly in part 2,
    but it seems like I'm finally getting the right intuition for avoiding
    those.
    
    It was _really_ convenient that I happened to have a breadth-first-search
    navigation function lying around. (Docs say it's A*, but a quick glance
    at the source implies that it doesn't use a heuristic function, so
    it really shouldn't be called that.)
    
    Actually, this whole exercise felt like a huge validation of having
    a generic tile cartography library prepped and ready for AoC. There
    are many more than 100 lines of code _involved_, but I didn't have to
    write that many _today_.

---
+++ day13/src/lib.rs
@@ -0,0 +1,91 @@
+use aoclib::{
+    geometry::{
+        map::{ContextFrom, ContextInto, Map, Traversable},
+        Point,
+    },
+    parse,
+};
+use std::{
+    collections::{HashSet, VecDeque},
+    path::Path,
+};
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+struct Tile(bool);
+
+impl ContextFrom<Tile> for Traversable {
+    type Context = ();
+
+    fn ctx_from(Tile(is_wall): Tile, _context: &Self::Context) -> Self {
+        match is_wall {
+            true => Traversable::Obstructed,
+            false => Traversable::Free,
+        }
+    }
+}
+
+fn make_cubical_design(favorite_number: i32) -> impl Fn(Point) -> Tile {
+    move |Point { x, y }: Point| -> Tile {
+        let mut magic = x * x + 3 * x + 2 * x * y + y + y * y;
+        magic += favorite_number;
+        Tile(magic.count_ones() % 2 != 0)
+    }
+}
+
+fn make_map(edge_size: usize, favorite_number: i32) -> Map<Tile> {
+    Map::procedural(edge_size, edge_size, make_cubical_design(favorite_number))
+}
+
+const INITIAL: Point = Point::new(1, 1);
+const PART1_GOAL: Point = Point::new(31, 39);
+
+pub fn part1(input: &Path) -> Result<(), Error> {
+    for favorite_number in parse::<i32>(input)? {
+        let map = make_map(64, favorite_number);
+        let path_len = map
+            .navigate(INITIAL, PART1_GOAL)
+            .ok_or_else(|| Error::NoPath(INITIAL, PART1_GOAL))?
+            .len();
+        println!("number of steps from initial to goal: {}", path_len);
+    }
+    Ok(())
+}
+
+pub fn part2(input: &Path) -> Result<(), Error> {
+    for favorite_number in parse::<i32>(input)? {
+        let map = make_map(64, favorite_number);
+
+        let mut visited = HashSet::new();
+        visited.insert(INITIAL);
+        let mut queue = VecDeque::new();
+        queue.push_front((0, INITIAL));
+
+        while let Some((steps, position)) = queue.pop_front() {
+            if steps > 50 {
+                continue;
+            }
+
+            map.orthogonal_adjacencies(position)
+                .filter(|adj| {
+                    !visited.contains(adj) && {
+                        let traversable: Traversable = map[*adj].ctx_into(&());
+                        traversable == Traversable::Free
+                    }
+                })
+                .for_each(|adj| queue.push_back((steps + 1, adj)));
+
+            visited.insert(position);
+        }
+
+        println!("reachable positions in 50 steps: {}", visited.len());
+    }
+    Ok(())
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[error("no path found from {0:?} to {1:?}")]
+    NoPath(Point, Point),
+}
