36b531d61c1522db1089a81334ebd9a62ec3a8e5
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-06-14 23:35:40 +0200

    partially implement day22 part 2
    The code is atrocious, just stream-of-consciousness naive shit that
    certainly needs to be optimized.
    
    Step 1: can I possibly do this without copying the map all over
    the place? (How? Compute data_from dynamically according to the move set?)
    
    Step 2: filter out already-visited states, of course.

--- day22/src/lib.rs
+++ day22/src/lib.rs
@@ -1,9 +1,12 @@
-use aoclib::geometry::Point;
+use aoclib::geometry::{Direction, Point};
 use regex::Regex;
 use std::{
+    collections::{HashMap, VecDeque},
     convert::{TryFrom, TryInto},
     io::BufRead,
+    ops::IndexMut,
     path::Path,
+    rc::Rc,
     str::FromStr,
 };
 
@@ -94,6 +97,137 @@ impl TryFrom<RawNode> for Node {
     }
 }
 
+#[derive(Debug, PartialEq, Eq, Clone)]
+struct MapNode {
+    size: u32,
+    used: u32,
+    // strictly speaking, this should be a hashset or something, but
+    // I'd be very surprised if we ever ended up with a vector more than
+    // a handful of items long, so it's probably fine to perform a linear search here.
+    data_from: Vec<Point>,
+}
+
+impl From<Node> for MapNode {
+    fn from(
+        Node {
+            position,
+            size,
+            used,
+        }: Node,
+    ) -> Self {
+        MapNode {
+            size,
+            used,
+            data_from: vec![position],
+        }
+    }
+}
+
+impl MapNode {
+    fn avail(&self) -> u32 {
+        self.size - self.used
+    }
+
+    fn move_into(&mut self, other: &mut MapNode) {
+        other.used += self.used;
+        self.used = 0;
+        other.data_from.extend(self.data_from.drain(..));
+    }
+}
+
+type Map = aoclib::geometry::Map<MapNode>;
+
+fn make_map(input: &Path) -> Result<Map, Error> {
+    let nodes: HashMap<_, MapNode> = parse(input)?
+        .map(|node| (node.position, node.into()))
+        .collect();
+    let max_x = nodes
+        .keys()
+        .map(|position| position.x)
+        .max()
+        .ok_or(Error::NoInput)?;
+    let max_y = nodes
+        .keys()
+        .map(|position| position.y)
+        .max()
+        .ok_or(Error::NoInput)?;
+    Ok(Map::procedural(
+        max_x as usize,
+        max_y as usize,
+        |position| {
+            nodes
+                .get(&position)
+                .expect("input covers all points in map")
+                .clone()
+        },
+    ))
+}
+
+struct State {
+    map: Map,
+    // from parent map, move data from point in direction
+    parent: Option<(Rc<State>, Point, Direction)>,
+}
+
+impl State {
+    fn is_target(&self) -> bool {
+        self.map[self.map.bottom_left()]
+            .data_from
+            .contains(&self.map.bottom_right())
+    }
+
+    fn children(self) -> Vec<State> {
+        let state_parent = Rc::new(self);
+        let mut children = Vec::new();
+
+        state_parent.map.for_each_point(|node, point| {
+            for neighbor_point in state_parent.map.orthogonal_adjacencies(point) {
+                let neighbor = std::ops::Index::index(&state_parent.map, point);
+                if neighbor.avail() >= node.used {
+                    let mut map = state_parent.map.clone();
+                    let mut neighbor = neighbor.to_owned();
+                    map.index_mut(point).move_into(&mut neighbor);
+                    map[neighbor_point] = neighbor;
+
+                    children.push(State {
+                        map,
+                        parent: Some((
+                            state_parent.clone(),
+                            point,
+                            (neighbor_point - point).try_into().expect(
+                                "orthogonal adjacencies should always convert to a direction",
+                            ),
+                        )),
+                    })
+                }
+            }
+        });
+
+        children
+    }
+
+    fn steps_to(&self) -> usize {
+        self.parent
+            .as_ref()
+            .map(|(parent, _, _)| parent.steps_to() + 1)
+            .unwrap_or_default()
+    }
+}
+
+fn breadth_first_search(map: Map) -> Option<State> {
+    let mut queue = VecDeque::new();
+    queue.push_back(State { map, parent: None });
+
+    while let Some(state) = queue.pop_front() {
+        if state.is_target() {
+            return Some(state);
+        }
+        queue.extend(state.children())
+    }
+
+    None
+}
+
 pub fn part1(input: &Path) -> Result<(), Error> {
     let mut nodes: Vec<_> = parse(input)?.collect();
     nodes.sort_unstable_by_key(|node| node.avail());
@@ -111,8 +245,11 @@ pub fn part1(input: &Path) -> Result<(), Error> {
     Ok(())
 }
 
-pub fn part2(_input: &Path) -> Result<(), Error> {
-    unimplemented!()
+pub fn part2(input: &Path) -> Result<(), Error> {
+    let map = make_map(input)?;
+    let solution_state = breadth_first_search(map).ok_or(Error::NoSolution)?;
+    println!("min steps to solution: {}", solution_state.steps_to());
+    Ok(())
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -123,4 +260,8 @@ pub enum Error {
     ParseErr,
     #[error("RawNode is not valid: {0:#?}")]
     Invalid(RawNode),
+    #[error("no input")]
+    NoInput,
+    #[error("could not find path to get goal data to origin node")]
+    NoSolution,
 }
